88c000c4a4643a2bdec7457cd690cdbb3e179a14
Salsa-Dude <arias.joseph.a@gmail.com>
2019-10-28 12:24:02 -0400

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(tutorial) {
+    let sentence = tutorial.split(" ")
+    let capitalizedSentence = sentence.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    })
+    return capitalizedSentence.join(" ")
+  })
 }
+
+
